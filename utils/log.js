965c498c55e7fd0620096e011cd688b3e98e54a0
Robert Detjens <detjensrobert@gmail.com>
2020-02-23 15:24:00 -0800

    simplify log file

--- utils/log.js
+++ utils/log.js
@@ -1,26 +1,36 @@
 const c = require('chalk');
 
-function log(type, message) {
+function buildStr (type, message) {
 	const d = new Date();
-	let str = " ";
+	let str;
+	
 	switch (type) {
-	case 'START':
-		str += c.cyan(type);
-		break;
-	case 'INFO':
-		str += c.green(type);
-		break;
-	case 'WARN':
-		str += c.yellow(type);
-		break;
-	case 'ERR!':
-		str += c.red(type);
-		break;
-	default:
-		str += c.grey(type);
+		case 'START':
+			str = c.cyan(type);
+			break;
+		case 'INFO':
+			str = c.green(type);
+			break;
+		case 'WARN':
+			str = c.yellow(type);
+			break;
+		case 'ERR':
+			str = c.red(type);
+			break;
+		default:
+			str = c.grey(type);
 	}
+	
+	return "[ " + c.grey(d.toLocaleString()) + " | " + str + " ] " + message;
+}
 
-	console.log(c.white("[ ") + c.grey(d.toLocaleDateString()) + " " + c.grey(d.toLocaleTimeString()) + str + c.white(" ] ") + c.reset(message));
+function log (type, message) {
+	console.log(buildStr(type, message));
+}
+
+function error(type, message) {
+	console.error(buildStr(type, message));
 }
 
 module.exports.log = log;
+module.exports.error = error;
